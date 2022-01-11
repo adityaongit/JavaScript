a8e63a443d5a4ae00a5c4dcb89381bd72af9c696
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-11 19:28:57 +0530

    objects theory added

--- object2.js
+++ object2.js
@@ -1,8 +1,19 @@
 var user = {
-    firstName: "Aditya",
-    lastName: "Jindal",
-    Role: "Admin",
-    loginCount: 32,
-    googleSignedIn: true,
-  };
-  
+  firstName: "Aditya",
+  lastName: "Jindal",
+  Role: "Admin",
+  loginCount: 32,
+  googleSignedIn: true,
+  courseList: [],
+  buyCourse: function (courseName) {
+    this.courseList.push(courseName);
+  },
+  getCourseCount: function () {
+    return `${this.firstName} is enrolled in ${this.courseList.length} courses!`;
+  },
+};
+
+user.buyCourse("MERN Stack Course");
+console.log(user.getCourseCount());
+user.buyCourse("DevOps Bootcamp");
+console.log(user.getCourseCount());
