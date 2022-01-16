df306d7ee45f14bca1a4691758297582d38c0ef6
Aditya Jindal <raghavjindal017@gmail.com>
2022-01-16 22:34:33 +0530

    express used to route

--- Node.js/app.js
+++ Node.js/app.js
@@ -1,4 +1,18 @@
-const http = require("http");
+// const http = require("http");
+const express = require("express");
+const app = express();
+
+app.get("/currenttime", (req, res) => {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:3000/currenttime
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+app.listen(3000);
+
+/*
+Without Express
 
 function handleRequest(request, response) {
   if (request.url === "/currenttime") {
@@ -12,3 +26,4 @@ function handleRequest(request, response) {
 
 const server = http.createServer(handleRequest);
 server.listen(3000);
+*/
